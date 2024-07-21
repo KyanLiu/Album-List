761ffd010cc8aaf0de9d7065f3fc53fe5b0708a4
kyan <tbnrthunder9@gmail.com>
2024-07-21 16:50:17 -0400

    Prompt Login on Profile Page before logging in

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import guestPicture from '../../assets/img/guest.png';
 import AlbumList from '../../components/AlbumList/AlbumList';
 import {useContext} from 'react';
 import {UserLogin} from '../../App';
+import {Link} from 'react-router-dom';
 
 const Profile = () => {
     const {loggedIn, setLoggedIn, username, setUsername} = useContext(UserLogin);
@@ -10,23 +11,46 @@ const Profile = () => {
     const profilePicture = '';
     const about = 'Hello :3';
     const joinDate = 'MM/DD/YY';
+
+    /*const fetchTrackDetails = async (albumID) => {
+        try {
+            const response = await axios.get(`https://api.spotify.com/v1/albums/${albumID}`, {
+                headers: {
+                    'Authorization': `Bearer ${ACCESS_TOKEN}`
+                }
+            })
+            return response.data;
+        }
+        catch (error) {
+            console.error('Error fetching album tracklist', error);
+        }
+    }*/
     return (
         <div className="profileContainer">
-            <div className='profileAbout'>
-                <h1 className='profileUsername'>{profileUsername}</h1>
-                <img src={guestPicture} className='imgProp'></img>
-                <div className='aboutTag'>
-                    <p>{about}</p>
+            { loggedIn ? (
+                <div>
+                    <div className='profileAbout'>
+                        <h1 className='profileUsername'>{profileUsername}</h1>
+                        <img src={guestPicture} className='imgProp'></img>
+                        <div className='aboutTag'>
+                            <p>{about}</p>
+                        </div>
+                        <div className='aboutTag'>
+                            <p>Joined:</p>
+                            <p>{joinDate}</p>
+                        </div>
+                    </div>
+                    <div className='listContainer'>
+                        <h1 className='listTitle'>Albums to listen to:</h1>
+                        <AlbumList />
+                    </div>
                 </div>
-                <div className='aboutTag'>
-                    <p>Joined:</p>
-                    <p>{joinDate}</p>
+            ) : (
+                <div>
+                    <p>Please Login First</p>
+                    <Link to="/">Login</Link>
                 </div>
-            </div>
-            <div className='listContainer'>
-                <h1 className='listTitle'>Albums to listen to:</h1>
-                <AlbumList />
-            </div>
+            )}
         </div>
     )
 }
